87f0a11c4d2ddab75e386ce89b311f2e0b8134b0
Shawn Presser <shawnpresser@gmail.com>
2020-04-25 02:14:10 -0700

    Print gin config diff as json string

--- discord_tensorboard.py
+++ discord_tensorboard.py
@@ -51,6 +51,155 @@ async def send_picture(channel, img, kind='png', name='test', text=None):
     picture.filename = name + '.' + kind
     await channel.send(content=text, file=picture)
 
+def _get_tensors(event_acc, names):
+  if not isinstance(names, list):
+    names = [names]
+  tags = event_acc.Tags()
+  for tensor in tags.get('tensors', []):
+    if tensor in names:
+      for event in event_acc.tensors.Items(tensor):
+        props = dict([(k.name, v) for k, v in event.tensor_proto.ListFields()])
+        props['event'] = event
+        props['tensor'] = tensor
+        yield props
+
+def get_tensors(event_acc, names=['gin/operative_config'], step=None):
+  results = list(sorted(_get_tensors(event_acc, names), key=lambda x: x['event'].step))
+  if step is not None and len(results) > 0:
+    for result in results:
+      if result['event'].step >= step:
+        return result
+    return results[-1]
+  return results
+
+def get_string_val(x, unset=None):
+  if x is None:
+    return unset
+  return x['string_val'][0].decode('utf8')
+
+def get_config(event_acc, step, description=None, match=None, exclude=None):
+  result = get_tensors(event_acc, 'gin/operative_config', step=step)
+  if result is None:
+    return None
+  cfg = get_string_val(result)
+  if cfg is None:
+    cfg = "No config"
+  if match is not None:
+    if exclude is None:
+      exclude = []
+    if not isinstance(match, list):
+      match = [match]
+    if not isinstance(exclude, list):
+      exclude = [exclude]
+    cfg = '\n'.join([x for x in cfg.splitlines() if any([x.lstrip().startswith(y) for y in match]) and not any([x.lstrip().startswith(y) for y in exclude])])
+    return cfg
+  if description is not None:
+    description = " " + description
+  else:
+    description = ""
+  cfg = "{} at step {}{}\n{}".format(result['tensor'], result['event'].step, description, cfg)
+  return cfg
+
+def get_settings(event_acc, step):
+  return [x.strip().split(' = ', 1) for x in get_config(event_acc, step=step, match='', exclude=''.split()).splitlines() if not x.strip().startswith('#') and len(x.strip()) > 0]
+
+biggan_defaults = dict([
+ ['AdamOptimizer.beta1', '0.0'],
+ ['AdamOptimizer.beta2', '0.999'],
+ ['AdamOptimizer.epsilon', '1e-08'],
+ ['AdamOptimizer.use_locking', 'False'],
+ ['BigGanResNetBlock.add_shortcut', 'True'],
+ ['conditional_batch_norm.use_bias', 'False'],
+ ['cross_replica_moments.group_size', 'None'],
+ ['cross_replica_moments.parallel', 'True'],
+ ['D.batch_norm_fn', 'None'],
+ ['D.layer_norm', 'False'],
+ ['D.spectral_norm', 'True'],
+ ['dataset.name', ""],
+ ['dataset.seed', '547'],
+ ['resnet_biggan.Discriminator.blocks_with_attention', ""],
+ ['resnet_biggan.Discriminator.ch', ''],
+ ['resnet_biggan.Discriminator.channel_multipliers', 'None'],
+ ['resnet_biggan.Discriminator.project_y', 'True'],
+ ['G.batch_norm_fn', ''], #'@conditional_batch_norm'],
+ ['G.spectral_norm', 'True'],
+ ['resnet_biggan.Generator.blocks_with_attention', ""],
+ ['resnet_biggan.Generator.ch', ''],
+ ['resnet_biggan.Generator.channel_multipliers', 'None'],
+ ['resnet_biggan.Generator.embed_bias', 'False'],
+ ['resnet_biggan.Generator.embed_y', 'True'],
+ ['resnet_biggan.Generator.embed_y_dim', '128'],
+ ['resnet_biggan.Generator.embed_z', 'False'],
+ ['resnet_biggan.Generator.hierarchical_z', 'True'],
+ ['loss.fn', '@hinge'],
+ ['ModularGAN.conditional', 'True'],
+ ['ModularGAN.d_lr', ''],
+ ['ModularGAN.d_optimizer_fn', '@tf.train.AdamOptimizer'],
+ ['ModularGAN.deprecated_split_disc_calls', 'False'],
+ ['ModularGAN.ema_decay', '0.9999'],
+ ['ModularGAN.ema_start_step', '40000'],
+ ['ModularGAN.experimental_force_graph_unroll', 'False'],
+ ['ModularGAN.experimental_joint_gen_for_disc', 'False'],
+ ['ModularGAN.fit_label_distribution', 'False'],
+ ['ModularGAN.g_lr', ''],
+ ['ModularGAN.g_optimizer_fn', '@tf.train.AdamOptimizer'],
+ ['ModularGAN.g_use_ema', 'True'],
+ ['normal.mean', '0.0'],
+ ['normal.seed', 'None'],
+ ['options.architecture', "'resnet_biggan_arch'"],
+ ['options.batch_size', ''],
+ ['options.d_flood', ''],
+ ['options.datasets', ""],
+ ['options.description', ""],
+ ['options.disc_iters', '2'],
+ ['options.discriminator_normalization', 'None'],
+ ['options.g_flood', ''],
+ ['options.gan_class', '@ModularGAN'],
+ ['options.image_grid_height', '32'],
+ ['options.image_grid_resolution', '1024'],
+ ['options.image_grid_width', '32'],
+ ['options.labels', 'None'],
+ ['options.lamba', '1'],
+ ['options.model_dir', ""],
+ ['options.num_classes', '1000'],
+ ['options.random_labels', ''],
+ ['options.training_steps', '250000'],
+ ['options.transpose_input', 'False'],
+ ['options.z_dim', ''],
+ ['penalty.fn', '@no_penalty'],
+ ['replace_labels.file_pattern', 'None'],
+ ['run_config.iterations_per_loop', '250'],
+ ['run_config.keep_checkpoint_every_n_hours', '1'],
+ ['run_config.keep_checkpoint_max', '10'],
+ ['run_config.save_checkpoints_steps', '250'],
+ ['run_config.single_core', 'False'],
+ ['run_config.tf_random_seed', 'None'],
+ ['spectral_norm.epsilon', '1e-12'],
+ ['spectral_norm.singular_value', "'auto'"],
+ ['standardize_batch.decay', '0.9'],
+ ['standardize_batch.epsilon', '1e-05'],
+ ['standardize_batch.use_cross_replica_mean', 'None'],
+ ['standardize_batch.use_moving_averages', 'False'],
+ ['train_imagenet_transform.crop_method', "'random'"],
+ ['weights.initializer', "'orthogonal'"],
+ ['z.distribution_fn', '@tf.random.normal'],
+ ['z.maxval', '1.0'],
+ ['z.minval', '-1.0'],
+ ['z.stddev', '1.0']
+ ])
+
+def get_settings_diff(event_acc, step, exclude=biggan_defaults):
+  return [(k, v) for k, v in get_settings(event_acc, step) if exclude.get(k) != v]
+
+import json
+
+def get_description(event_acc, step):
+  #result = get_config(event_acc, step=step, match='options', exclude='options.image_ options.transpose_input options.training_steps'.split())
+  result = get_settings_diff(event_acc, step)
+  if result is None:
+    return ""
+  return json.dumps(dict(result))
+
 def get_images(event_acc=None, name='fake_images_image_0'):
   if event_acc is None:
     print("Loading event accumulator for {}".format(args.logdir))
@@ -58,15 +207,21 @@ def get_images(event_acc=None, name='fake_images_image_0'):
   print("Reloading event accumulator for {}".format(args.logdir))
   event_acc.Reload()
   print("Finished loading event accumulator for {}".format(args.logdir))
-  for tag in event_acc.Tags()['images']:
+  tags = event_acc.Tags()
+  configs = sorted(get_text(event_acc), key=lambda x: x['event'].step)
+  for tag in tags['images']:
       events = event_acc.Images(tag)
-
       tag_name = tag.replace('/', '_')
       if tag_name == name:
           for index, event in enumerate(events):
               s = np.frombuffer(event.encoded_image_string, dtype=np.uint8)
               bytes_io = bytearray(s)
               img = Image.open(io.BytesIO(bytes_io))
+              cfgs = [x for x in configs if x['event'].step >= event.step]
+              if len(cfgs) >= 0:
+                cfg = cfgs[0]
+                cfg.get('string_val', [b''])
+              config = None if len(config) <= 0 else config[0]
               yield index, img, event
 
 def bot(channel_name, name='test', kind='png'):
@@ -102,7 +257,10 @@ def bot(channel_name, name='test', kind='png'):
                       lastevent = event.wall_time
                       if index == 0:
                           start_time = event.wall_time
-                      text = "```#{} step {} elapsed {:.2f}m\n{}\n{}```".format(index, event.step, (event.wall_time - start_time)/60.0, timestamp(event.wall_time), args.logdir)
+                      desc = get_description(event_acc, step=event.step)
+                      if len(desc.strip()) > 0:
+                        desc = '\n' + desc.strip()
+                      text = "```#{} step {} elapsed {:.2f}m\n{}\n{}{}```".format(index, event.step, (event.wall_time - start_time)/60.0, timestamp(event.wall_time), args.logdir, desc)
                       print(text)
                       if index >= args.start and (args.end is None or index <= args.end):
                           args.start = index + 1
